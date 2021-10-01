e2bab98327bdfcf16bea8db082719ea78b6c15a6
Sameer Pusegaonkar <sameer@umd.edu>
2021-10-01 11:30:45 -0400

    Setup the main file

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,14 @@
-#include <iostream>
+/**
+ * @file main.cpp
+ * @author Sameer Pusegaonkar (sameer@umd.edu) - Driver, Kavyashree Devadiga (kavya@umd.edu) - Navigator 
+ * @brief A program to compute error for a PID control system.
+ * @version 0.1
+ * @date 2021-09-19
+ * @copyright A dummy copyright message 
+ * 
+ */
+
+#include "PID.h"
 
 int main() {
     return 0;
