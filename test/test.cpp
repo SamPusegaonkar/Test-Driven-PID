ad566e4a361c45b4c065a8bec55ae2ccc70d9a75
Sameer Pusegaonkar <sameer@umd.edu>
2021-10-01 18:48:36 -0400

    Add more tests for verifying ComputeError method

--- test/test.cpp
+++ test/test.cpp
@@ -4,7 +4,6 @@
  * @brief A file to verify the methods of the PID class
  * @version 0.1
  * @date 2021-10-01
- * 
  * @copyright Copyright (c) 2021
  * 
  */
@@ -15,12 +14,12 @@
 /**
  * @brief Tests the default constructor
 */
-// TEST(Test_Driven_PID, test_the_default_constructor) {
-//   PID pid_controller;
-//   EXPECT_EQ(pid_controller.GetKP(), 0);
-//   EXPECT_EQ(pid_controller.GetKD(), 0);
-//   EXPECT_EQ(pid_controller.GetKI(), 0);
-// }
+TEST(Test_Driven_PID, test_the_default_constructor) {
+  PID pid_controller;
+  EXPECT_EQ(pid_controller.GetKP(), 0);
+  EXPECT_EQ(pid_controller.GetKD(), 0);
+  EXPECT_EQ(pid_controller.GetKI(), 0);
+}
 
 /**
  * @brief Tests PID controller proportional gain, keeping differential
@@ -31,33 +30,54 @@ TEST(Test_Driven_PID, test_with_kp) {
   pid_controller.SetKP(0.9);
   pid_controller.SetKD(0);
   pid_controller.SetKI(0);
-  //  EXPECT_NEAR(pid_controller.ComputeError(12, 10), 1.8, 0.001);
-  EXPECT_NEAR(1.8, 1.8, 0.001);
-
+  EXPECT_NEAR(pid_controller.ComputeError(12, 10), 1.8, 0.001);
 }
 
 /**
  * @brief Tests PID controller proportional and integral gain keeping differential
  * constant zero
 */
-// TEST(Test_Driven_PID, test_with_kp_ki) {
-//   PID pid_controller;
-//   pid_controller.SetKP(0.3);
-//   pid_controller.SetKD(0);
-//   pid_controller.SetKI(0.2);
-//   /**
-//    * Iteration 1
-//    * (Δe) = 10 - 5 = 5
-//    * ep = (0.35) = 1.5  && ei = (0.2)(5/1) = 1
-//    * (Δe) = 2.5 _____________ [5,2.5]
-//   */
-//   EXPECT_NEAR(pid_controller.ComputeError(10, 5), 2.5, 0.001);
-//   /**
-//    * Iteration 2
-//    * (Δe) = 2.5
-//    * ep = (0.32.5)  && ei = (0.2)(5+2.5/2)
-//    * (Δe) = 1.5 _____________ [5,2.5,1.5]
-//   */
-//   EXPECT_NEAR(pid_controller.ComputeError(10, 7.5), 1.5, 0.01);
-// }
+TEST(Test_Driven_PID, test_with_kp_ki) {
+  PID pid_controller;
+  pid_controller.SetKP(0.3);
+  pid_controller.SetKD(0);
+  pid_controller.SetKI(0.2);
+  /**
+   * Iteration 1
+   * (Δe) = 10 - 5 = 5
+   * ep = (0.3*5) = 1.5  && ei = (0.2)(5/1) = 1
+   * (Δe) = 2.5 _____________ [5,2.5]
+  */
+  EXPECT_NEAR(pid_controller.ComputeError(10, 5), 2.5, 0.001);
+  /**
+   * Iteration 2
+   * (Δe) = 2.5
+   * ep = (0.3*2.5)  && ei = (0.2)(5+2.5/2)
+   * (Δe) = 1.5 _____________ [5,2.5,1.5]
+  */
+  EXPECT_NEAR(pid_controller.ComputeError(10, 7.5), 1.5, 0.01);
+}
 
+/**
+ * @brief Tests PID controller with proportional, integral and differential gain
+*/
+TEST(Test_Driven_PID, test_with_kp_ki_kd) {
+  PID pid_controller;
+  pid_controller.SetKP(0.3);
+  pid_controller.SetKD(0.2);
+  pid_controller.SetKI(0.01);
+  /**
+   * Iteration 1
+   * (Δe) = 10 - 5 = 5
+   * ep = (0.3*5) = 1.5  && ei = (0.01)(5/1) = 0.05 && ed = 0 (no previous iteration)
+   * (Δe) = 1.55 _____________ [5,1.55]
+  */
+  EXPECT_NEAR(pid_controller.ComputeError(10, 5), 1.55, 0.01);
+  /**
+   * Iteration 2
+   * (Δe) = 1.55
+   * ep = (0.3*1.55)  && ei = (0.01)(5+1.55/2) && ed = 0.2(5-1.55)
+   * (Δe) = 1.5 _____________ [5,2.5,1.5]
+  */
+  EXPECT_NEAR(pid_controller.ComputeError(10, 6.55), 1.18775, 0.00001);
+}
