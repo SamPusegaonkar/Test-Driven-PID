8ab4a8910b97fd11780a88e30bf16a88fbacf198
Sameer Pusegaonkar <sameer@umd.edu>
2021-10-01 12:24:42 -0400

    Create tests for the PID class

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,26 @@
 #include <gtest/gtest.h>
+#include "PID.h"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+/**
+ * @brief Tests the default constructor
+*/
+TEST(Test_Driven_PID, test_the_default_constructor) {
+  PID pid_controller;
+  EXPECT_EQ(pid_controller.GetKP(), 0);
+  EXPECT_EQ(pid_controller.GetKD(), 0);
+  EXPECT_EQ(pid_controller.GetKI(), 0);
 }
+
+/**
+ * @brief Tests PID controller proportional gain keeping differential
+ * and integral constants zero
+*/
+TEST(Test_Driven_PID, test_with_kp) {
+  PID pid_controller;
+  pid_controller.SetKP(0.9);
+  pid_controller.SetKD(0);
+  pid_controller.SetKI(0);
+  EXPECT_NEAR(pid_controller.ComputeError(12, 10), 1.8, 0.001);
+}
+
+
