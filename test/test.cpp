d62f58685e9b53e9cfc4da31cb7598ee379af120
Sameer Pusegaonkar <sameer@umd.edu>
2021-10-02 16:32:05 -0400

    Fix the incorrect computation in the test cases

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,11 @@
 /**
  * @file test.cpp
- * @author Sameer Pusegaonkar (sameer@umd.edu) - Driver, Kavyashree Devadiga (kavya@umd.edu) - Navigator 
+ * @author Sameer Pusegaonkar (sameer@umd.edu) - Driver, Kavyashree Devadiga (kavya@umd.edu) - Navigator
  * @brief A file to verify the methods of the PID class
  * @version 0.1
  * @date 2021-10-01
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #include <gtest/gtest.h>
@@ -46,14 +46,14 @@ TEST(Test_Driven_PID, test_with_kp_ki) {
    * Iteration 1
    * (Δe) = 10 - 5 = 5
    * ep = (0.3*5) = 1.5  && ei = (0.2)(5/1) = 1
-   * (Δe) = 2.5 _____________ [5,2.5]
+   * (Δe) = 2.5 _____________ [2.5]
   */
   EXPECT_NEAR(pid_controller.ComputeError(10, 5), 2.5, 0.001);
   /**
    * Iteration 2
    * (Δe) = 2.5
-   * ep = (0.3*2.5)  && ei = (0.2)(5+2.5/2)
-   * (Δe) = 1.5 _____________ [5,2.5,1.5]
+   * ep = (0.3*2.5)  && ei = (0.2)(2.5+2.5/2)
+   * (Δe) = 1.25 _____________ [2.5,1.25]
   */
   EXPECT_NEAR(pid_controller.ComputeError(10, 7.5), 1.5, 0.01);
 }
@@ -70,14 +70,14 @@ TEST(Test_Driven_PID, test_with_kp_ki_kd) {
    * Iteration 1
    * (Δe) = 10 - 5 = 5
    * ep = (0.3*5) = 1.5  && ei = (0.01)(5/1) = 0.05 && ed = 0 (no previous iteration)
-   * (Δe) = 1.55 _____________ [5,1.55]
+   * (Δe) = 1.55 _____________ [1.55]
   */
   EXPECT_NEAR(pid_controller.ComputeError(10, 5), 1.55, 0.01);
   /**
    * Iteration 2
-   * (Δe) = 1.55
-   * ep = (0.3*1.55)  && ei = (0.01)(5+1.55/2) && ed = 0.2(5-1.55)
-   * (Δe) = 1.5 _____________ [5,2.5,1.5]
+   * (Δe) = 10-6.55 = 3.45
+   * ep = (0.3*3.45)  && ei = (0.01)(1.55+3.45/2) && ed = 0.2(3.45-1.55)
+   * (Δe) = 1.44 _____________ [1.44]
   */
-  EXPECT_NEAR(pid_controller.ComputeError(10, 6.55), 1.18775, 0.00001);
+  EXPECT_NEAR(pid_controller.ComputeError(10, 6.55), 1.44, 0.001);
 }
